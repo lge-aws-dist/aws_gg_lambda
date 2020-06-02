2cbb512840b4be9aeb159497094556874b92a68f
EunJung <lge-aws-dist@lge.com>
2020-06-02 18:55:45 +0900

    update to add license comment

--- greengrassML.py
+++ greengrassML.py
@@ -1,4 +1,18 @@
-import json
+'''
+ *   Copyright (c) 2020 LG Electronics Inc.
+ *
+ *   This program or software including the accompanying associated documentation
+ *   ("Software") is the proprietary software of LG Electronics Inc. and or its
+ *   licensors, and may only be used, duplicated, modified or distributed pursuant
+ *   to the terms and conditions of a separate written license agreement between you
+ *   and LG Electronics Inc. ("Authorized License"). Except as set forth in an
+ *   Authorized License, LG Electronics Inc. grants no license (express or implied),
+ *   rights to use, or waiver of any kind with respect to the Software, and LG
+ *   Electronics Inc. expressly reserves all rights in and to the Software and all
+ *   intellectual property therein. If you have no Authorized License, then you have
+ *   no rights to use the Software in any ways, and should immediately notify LG
+ *   Electronics Inc. and discontinue all use of the Software.
+'''
 import cv2
 import sys
 import time
@@ -15,15 +29,19 @@ from network.tinyyolo_lite import TinyYoloNet
 
 app = Flask(__name__)
 
+#ejlim
 # Setup logging to stdout
-logger = logging.getLogger(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+#logger = logging.getLogger(__name__)
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+# Setup logging to stdout
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 client = greengrasssdk.client('iot-data')
 
 select_network = '0'
 answer = 'No data'
-view = 'off'
 
 def get_input_topic(context):
 	try:
@@ -32,9 +50,9 @@ def get_input_topic(context):
 		logging.error('Topic could not be parsed. ' + repr(e))
 	return topic
 
-def get_input_message(event, key):
+def get_input_message(event):
 	try:
-		message = event[key]
+		message = event['network']
 	except Exception as e:
 		logging.error('Message could not be parsed. ' + repr(e))
 	return message
@@ -47,8 +65,6 @@ def run_lne():
 	try:
 		global select_network
 		global answer
-		global view
-		
 		lne_mtcnn = Mtcnn('./lib/libdq1face.so')
 		lne_mobile = MobileNet('/home/ubuntu/models/mobilenet.lne', './labels/labels.txt')
 		lne_tinyyolo = TinyYoloNet('/home/ubuntu/models/tiny-yolo.lne')
@@ -57,19 +73,19 @@ def run_lne():
 		while(cap.isOpened()):
 			ret, frame = cap.read()
 			frame = camera_convert(frame)
-			if select_network == '1': # mtcnn : face detection
+			if select_network == '1':
 				input_img = lne_mtcnn.resize_img(frame)
 				answer_mtcnn = lne_mtcnn.inference(input_img)
 				answer = str(answer_mtcnn)
 				network_img = lne_mtcnn.post_draw(input_img, answer_mtcnn)
 	
-			elif select_network == '2': # mobilenet : object classification
+			elif select_network == '2':
 				input_img = lne_mobile.crop_img(frame)
 				input_img = lne_mobile.resize_img(input_img)
 				answer = lne_mobile.inference(input_img)
 				network_img = lne_mobile.post_draw(frame, answer)
 			
-			elif select_network == '3': # tiny yolo : object detection
+			elif select_network == '3':
 				input_img = lne_tinyyolo.crop_img(frame)
 				input_img = lne_tinyyolo.resize_img(input_img)
 				lne_result = lne_tinyyolo.inference(input_img)
@@ -83,11 +99,8 @@ def run_lne():
 				network_img = cv2.imencode('.jpg', frame)[1].tobytes()
 				answer = 'No data'
 
-			if view == "on":
-				yield (b'--frame\r\n'
-					b'Content-Type: image/jpeg\r\n\r\n' + network_img + b'\r\n')
-			else:
-				logging.debug('view off');
+			yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + network_img + b'\r\n')
 
 	except Exception as e:
 		logging.error(e)
@@ -95,7 +108,7 @@ def run_lne():
 def greengrass_ML():
 	global answer
 	while True:
-		client.publish(topic='lge/lg8111/greengrassML/inference/event', payload=answer)
+		client.publish(topic='lge/answer_topic', payload=answer)
 		time.sleep(1)
 
 
@@ -108,7 +121,7 @@ def video_feed():
 	return Response(run_lne(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def run_stream():
-	app.run(host='192.168.1.14', port='1234', debug=False)
+	app.run(host='0.0.0.0', port='1234', debug=False)
 
 def greengrass_run():
 	net_thread = threading.Thread(target=greengrass_ML)
@@ -122,20 +135,10 @@ def function_handler(event, context):
 	try:
 		global select_network
 		input_topic = get_input_topic(context)
-		input_message = get_input_message(event, 'select_network')
-
-		logging.debug("greengrassML receive topic : " + str(input_topic));
-		logging.debug("greengrassML received message : " + str(event));
-
-		if 'select_network' in event:
-			select_network = event['select_network'];
-			logging.debug('select_network' + select_network);
-			
-		if 'view' in event:
-			view = event['view'];
-			logging.debug('view' + view);
-		
-		client.publish(topic='lge/lg8111/greengrassML/inference/event', payload='{}, {}'.format(input_topic, select_network))
+		input_message = get_input_message(event)
+		if 'network' in input_topic:
+			select_network = input_message
+		client.publish(topic='lge/answer_topic', payload='{}, {}'.format(input_topic, select_network))
 	except Exception as e:
 		logging.error(e)
 
